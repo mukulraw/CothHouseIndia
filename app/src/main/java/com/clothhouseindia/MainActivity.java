6ad246731a2a44d26a06122287e12dac850fa136
mukul <mukulraw199517@gmail.com>
2016-09-21 18:40:13 +0530

    Debug Done

---
+++ app/src/main/java/com/clothhouseindia/MainActivity.java
@@ -0,0 +1,202 @@
+package com.clothhouseindia;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
+    private WebSettings webSettings;
+    private ProgressBar progress;
+    private ImageView image;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED , WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
+        image = (ImageView)findViewById(R.id.image);
+        progress = (ProgressBar)findViewById(R.id.progress);
+
+        onNewIntent(getIntent());
+
+        myWebView = (WebView)findViewById(R.id.webView);
+
+        if (myWebView != null) {
+            webSettings = myWebView.getSettings();
+        }
+
+        webSettings.setJavaScriptEnabled(true);
+
+        myWebView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        myWebView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        myWebView.getSettings().setAppCacheEnabled(true);
+        myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setSavePassword(true);
+        webSettings.setSaveFormData(true);
+        webSettings.setEnableSmoothTransition(true);
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+
+
+
+
+
+        //load the home page URL
+        myWebView.loadUrl("http://clothhouseindia.com/");
+        myWebView.setWebViewClient(new MyWebViewClient());
+        myWebView.setWebChromeClient(new MyWebChromeClient());
+        Animation anim = AnimationUtils.loadAnimation(getApplicationContext() , R.anim.fade_in);
+        image.startAnimation(anim);
+
+
+
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+            myWebView.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+            super.onReceivedError(view, request, error);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progress.setVisibility(View.GONE);
+        }
+    }
+
+    private class MyWebChromeClient extends WebChromeClient {
+
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+            progress.setProgress(newProgress);
+
+            progress.setVisibility(View.VISIBLE);
+
+
+
+
+
+
+
+
+
+
+            if (newProgress > 10)
+            {
+
+                image.animate().translationY(image.getHeight()).alpha(0.0f);
+                image.setVisibility(View.GONE);
+
+
+
+
+                myWebView.setVisibility(View.VISIBLE);
+                //progress.setVisibility(View.VISIBLE);
+            }
+
+            if (newProgress<100  && newProgress>18)
+            {
+                Log.d("asasd" , String.valueOf(newProgress));
+                progress.setVisibility(View.VISIBLE);
+
+
+            }
+            if (newProgress == 100)
+            {
+                progress.setVisibility(View.GONE);
+            }
+
+
+
+        }
+
+        //display alert message in Web View
+        @Override
+        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+            Log.d("asdasdasd", message);
+            new AlertDialog.Builder(view.getContext())
+                    .setMessage(message).setCancelable(true).show();
+            result.confirm();
+            return true;
+        }
+
+    }
+
+    private class JavaScriptInterface {
+
+        // Instantiate the interface and set the context
+        JavaScriptInterface() {
+        }
+
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+            myWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (myWebView.canGoBack())
+        {
+            myWebView.goBack();
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+
+    }
+}
